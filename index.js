6eb65e6c2fb40806a10b16cbedc5394f7aaad738
Yasamin <yasamin.amini98@gmail.com>
2024-08-06 15:53:33 +0200

    Comments added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+//1. Setting the initial background color to beige
 const body = document.getElementsByTagName("body")[0]
 body.style.backgroundColor = "beige"
-
+// 2. The setColor function changes the background color based on the color name passed to it
 function setColor(name){
     body.style.backgroundColor = name;
 }
-
+// 3. The randomColor function generates a random color and sets it as the background color
 function randomColor(){
     const red = Math.round(Math.random() * 255)
     const green = Math.round(Math.random() * 255)
@@ -13,5 +14,5 @@ function randomColor(){
     const color = `rgb(${red}, ${green}, ${blue})`
     body.style.backgroundColor = color;
 }
-
+// 4. The random color function is called once to set an initial random background color
 randomColor()
